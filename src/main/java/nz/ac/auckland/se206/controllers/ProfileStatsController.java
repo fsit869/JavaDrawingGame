79f263556c12ec9fae209609df90d1a4687e41df
tmur647 <tmur647@aucklanduni.ac.nz>
2022-09-18 16:32:34 +1200

    fixed formatting using mvn commands

--- src/main/java/nz/ac/auckland/se206/controllers/ProfileStatsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfileStatsController.java
@@ -12,33 +12,33 @@ import javafx.scene.image.ImageView;
 /** This is the controller for the profile stats view */
 public class ProfileStatsController {
 
-    ObservableList<String> fullWordHistory =
-            FXCollections.observableArrayList("Option 1", "Option 2", "Option 3");
-    @FXML private Label profileNameLabel;
-    @FXML private ImageView profileImageView;
+  ObservableList<String> fullWordHistory =
+      FXCollections.observableArrayList("Option 1", "Option 2", "Option 3");
+  @FXML private Label profileNameLabel;
+  @FXML private ImageView profileImageView;
 
-    @FXML private TextArea profileStatsTextArea;
+  @FXML private TextArea profileStatsTextArea;
 
-    @FXML private TextArea bestRecordsTextArea;
+  @FXML private TextArea bestRecordsTextArea;
 
-    @FXML private TextArea pastWordsTextArea;
+  @FXML private TextArea pastWordsTextArea;
 
-    @FXML private ComboBox<String> fullListComboBox;
+  @FXML private ComboBox<String> fullListComboBox;
 
-    public void initialize() {
-        fullListComboBox.setItems(fullWordHistory);
-    }
-    /////////////////////
-    // Button handlers //
-    /////////////////////
+  public void initialize() {
+    fullListComboBox.setItems(fullWordHistory);
+  }
+  /////////////////////
+  // Button handlers //
+  /////////////////////
 
-    /**
-     * This method is called when user clicks the main menu button to go back to main menu
-     *
-     * @param actionEvent Event of the button press
-     */
-    @FXML
-    private void onBackToMenuButton(ActionEvent actionEvent) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+  /**
+   * This method is called when user clicks the main menu button to go back to main menu
+   *
+   * @param actionEvent Event of the button press
+   */
+  @FXML
+  private void onBackToMenuButton(ActionEvent actionEvent) {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
 }
