db081ce584f92e62d4f2348b6f3852a929c49aaf
Javalind <65702538+Javalind@users.noreply.github.com>
2022-09-20 20:15:45 +1200

    added extra buttons, and added them into the controller.

--- src/main/java/nz/ac/auckland/se206/controllers/SelectProfilesController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SelectProfilesController.java
@@ -1,14 +1,34 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
+import nz.ac.auckland.se206.model.GameModel;
+import nz.ac.auckland.se206.profiles.ProfileFactory;
+import nz.ac.auckland.se206.profiles.entities.Profile;
 
 /** This class is responsible for selecting which profile to play */
 public class SelectProfilesController {
+  @FXML private Button profileOne;
+  @FXML private Button profileTwo;
+  @FXML private Button profileThree;
+  @FXML private Button profileFour;
+  @FXML private Button profileFive;
   @FXML private ButtonBar buttonBar;
 
-  public void initialize() {}
+  private List<Profile> profiles;
+
+  private Button[] arrButtons;
+
+  private ProfileFactory factory;
+  private GameModel gameModel;
+
+  public void initialize() {
+    this.arrButtons = new Button[5];
+    this.gameModel = GameModel.getInstance();
+  }
 
   /////////////////////
   // Button handlers //
