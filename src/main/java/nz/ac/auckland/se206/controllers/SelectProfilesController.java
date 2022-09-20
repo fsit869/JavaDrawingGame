9e60b1c8df8717a636062193229e36951d2c59d4
Javalind <65702538+Javalind@users.noreply.github.com>
2022-09-20 20:29:53 +1200

    added logic to determine whether or not a slot is empty or filled with a profile.

--- src/main/java/nz/ac/auckland/se206/controllers/SelectProfilesController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SelectProfilesController.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,8 +27,36 @@ public class SelectProfilesController {
   private GameModel gameModel;
 
   public void initialize() {
-    this.arrButtons = new Button[5];
+    setButtonsArray();
     this.gameModel = GameModel.getInstance();
+    // Catches any error thrown inside the factory
+    try {
+      this.profiles = factory.getAllProfiles();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    setButtons();
+  }
+
+  public void setButtons() {
+    for (int i = 0; i < this.profiles.size(); i++) {
+      if (profiles.get(i).getUsername() != "") {
+        this.arrButtons[i].setText(profiles.get(i).getUsername());
+      } else {
+        this.arrButtons[i].setText("New Profile");
+      }
+    }
+  }
+
+  /** Configures the buttons into an array */
+  public void setButtonsArray() {
+    this.arrButtons = new Button[5];
+    // All the buttons are put into this array
+    this.arrButtons[0] = profileOne;
+    this.arrButtons[1] = profileTwo;
+    this.arrButtons[2] = profileThree;
+    this.arrButtons[3] = profileFour;
+    this.arrButtons[4] = profileFive;
   }
 
   /////////////////////
@@ -40,7 +69,15 @@ public class SelectProfilesController {
    * @param actionEvent Event type of button
    */
   public void onNewProfileButton(ActionEvent actionEvent) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    Button current = (Button) actionEvent.getTarget();
+    // Check if button is creating new profile
+    if (current.getText().equals("New Profile")) {
+      gameModel.setCurrentViewState(GameModel.viewState.NEWPROFILE);
+    } else {
+      // In the case that profile already exists
+      gameModel.setProfile(factory.selectProfile(current.getText()));
+      gameModel.setCurrentViewState(GameModel.viewState.MAINMENU);
+    }
   }
 
   /**
