a199e3a905795cdbf27bd8d56ed556d1496b56eb
tmur647 <tmur647@aucklanduni.ac.nz>
2022-10-03 21:02:37 +1300

    added implementation so that instructions for each game settings show when hovered over

--- src/main/java/nz/ac/auckland/se206/controllers/gameModeSettingsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/gameModeSettingsController.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se206.controllers;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import nz.ac.auckland.se206.model.GameModel;
 import nz.ac.auckland.se206.profiles.ProfileFactory;
 
@@ -25,7 +26,8 @@ public class gameModeSettingsController implements ControllerInterface {
 
   @FXML private ComboBox<String> accuracyComboBox;
 
-  @FXML private ComboBox<String> difficultyComboBox;
+  @FXML private ComboBox<String> wordDifficultyComboBox;
+  @FXML private TextArea instructionsTextArea;
 
   public void initialize() {
 
@@ -35,6 +37,7 @@ public class gameModeSettingsController implements ControllerInterface {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+    instructionsTextArea.setText("Instructions");
 
     // Get game settings
     this.gameModel = GameModel.getInstance();
@@ -42,11 +45,7 @@ public class gameModeSettingsController implements ControllerInterface {
     confidenceComboBox.getItems().addAll(confidenceDifficulties);
     timeComboBox.getItems().addAll(timeDifficulties);
     accuracyComboBox.getItems().addAll(accuracyDifficulties);
-    difficultyComboBox.getItems().addAll(difficultyDifficulties);
-
-    //    timeComboBox.setItems(FXCollections.observableArrayList(timeDifficulties));
-    //    accuracyComboBox.setItems(FXCollections.observableArrayList(accuracyDifficulties));
-    //    difficultyComboBox.setItems(FXCollections.observableArrayList(difficultyDifficulties));
+    wordDifficultyComboBox.getItems().addAll(difficultyDifficulties);
   }
 
   @Override
@@ -70,6 +69,72 @@ public class gameModeSettingsController implements ControllerInterface {
   @FXML
   public void onLeftButton() {
     System.out.println("Not implemented yet");
+  }
+
+  @FXML
+  public void onConfidenceInfoEnter() {
+    instructionsTextArea.setText(
+        "Instructions-Confidence:\n"
+            + "Level easy: You win the game if the computer is at least 1% confident\n"
+            + "that your image is the word to guess.\n"
+            + "Level medium: You win the game if the computer is at least 10% confident\n"
+            + "that your image is the word to guess.\n"
+            + "Level hard: You win the game if the computer is at least 25% confident\n"
+            + "that your image is the word to guess.\n"
+            + "Level master: You win the game if the computer is at least 50% confident\n"
+            + "that your image is the word to guess.");
+  }
+
+  @FXML
+  public void onTimeInfoEnter() {
+    instructionsTextArea.setText(
+        "Instructions-Time limit:\n"
+            + "Level easy: You gets 60 seconds to draw the picture correctly.\n"
+            + "Level medium: You gets 45 seconds to draw the picture correctly.\n"
+            + "Level hard: You gets 30 seconds to draw the picture correctly.\n"
+            + "Level master: You gets 15 seconds to draw the picture correctly.");
+  }
+
+  @FXML
+  public void onAccuracyInfoEnter() {
+    instructionsTextArea.setText(
+        "Instructions-Accuracy:\n"
+            + "Level easy: You win the game if the word to draw is in the computer's top 3\n"
+            + "guesses.\n"
+            + "Level medium: You win the game if the word to draw is in the computer's top\n"
+            + "2 guesses.\n"
+            + "Level hard: You win the game if the word to draw is the computer’s best\n"
+            + "guess.");
+  }
+
+  @FXML
+  public void onWordDifficultyInfoEnter() {
+    instructionsTextArea.setText(
+        "Instructions-Word Difficulty:\n"
+            + "Level easy: You will only get given EASY words.\n"
+            + "Level medium: You will get given EASY or MEDIUM words.\n"
+            + "Level hard: You will get given EASY or MEDIUM or HARD words\n"
+            + "Level master: You will only get given HARD words.");
+  }
+
+  @FXML
+  public void onConfidenceInfoExit() {
+    instructionsTextArea.setText("Instructions");
+  }
+
+  @FXML
+  public void onTimeInfoExit() {
+    instructionsTextArea.setText("Instructions");
+  }
+
+  @FXML
+  public void onAccuracyInfoExit() {
+    instructionsTextArea.setText("Instructions");
+  }
+
+  @FXML
+  public void onWordDifficultyInfoExit() {
+    instructionsTextArea.setText("Instructions");
   }
 
   /** This method is called when user clicks the main menu button to go back to main menu */
